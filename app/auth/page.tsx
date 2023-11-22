7e3c6b9d311c83a4ba0415920faa65dca7014074
edm1001 <edmer_franciz@yahoo.com>
2023-11-21 21:43:17 -0600

    init welcome page

---
+++ app/auth/page.tsx
@@ -0,0 +1,13 @@
+const Auth = () => {
+    return (
+        <div className="relative w-full h-full bg-[url('/images/netflix-hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img src="/images/netflix-logo.png" alt="logo" className="h-12" />
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Auth;
